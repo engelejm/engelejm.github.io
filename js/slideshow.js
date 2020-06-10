63e6cef17bfad6e2f1d8950105d069def4913bd8
engelejm <60905631+engelejm@users.noreply.github.com>
2020-06-09 18:16:37 -0600

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,23 +2,12 @@
 var slideIndex = [1,1];
 showSlides(1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
 }
