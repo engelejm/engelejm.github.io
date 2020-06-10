5fc123eb8b07771ffefa5308eee0a6a50b78289f
engelejm <60905631+engelejm@users.noreply.github.com>
2020-06-09 18:43:05 -0600

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,4 @@
-// JavaScript Document
-var slideIndex = [1,1];
-showSlides(1);
+
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
@@ -10,4 +8,29 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginRight= "0";
+}
+
+// JavaScript Document
+var slideIndex = [1,1];
+showSlides(1);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
