abdd7b50dfc457435f75bef16619393860e1bb89
behmer92 <68659948+behmer92@users.noreply.github.com>
2021-04-14 19:47:21 -0400

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,13 +1,11 @@
-
+/* eslint-disable */
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginRight= "0";
 }
 
 // JavaScript Document
