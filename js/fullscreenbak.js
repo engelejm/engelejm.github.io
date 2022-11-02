1329a86b1fedd2c35d7556dc5fbe944cf1e65f3e
Eric Behm <edbehm@gmail.com>
2022-11-01 20:15:33 -0600

    fixed carousel

--- js/fullscreenbak.js
+++ js/fullscreenbak.js
@@ -1,24 +1,44 @@
 /*!
- * Flickity fullscreen v2.0.0
+ * Flickity fullscreen v1.1.1
  * Enable fullscreen view for Flickity
  */
 
+/*jshint browser: true, undef: true, unused: true, strict: true*/
+
 ( function( window, factory ) {
   // universal module definition
-  if ( typeof module == 'object' && module.exports ) {
+  /*jshint strict: false */ /*globals define, module, require */
+  if ( typeof define == 'function' && define.amd ) {
+    // AMD
+    define( [
+      'flickity/js/index',
+    ], factory );
+  } else if ( typeof module == 'object' && module.exports ) {
     // CommonJS
-    module.exports = factory( require('flickity') );
+    module.exports = factory(
+      require('flickity')
+    );
   } else {
     // browser global
-    factory( window.Flickity );
+    factory(
+      window.Flickity
+    );
   }
 
-}( typeof window != 'undefined' ? window : this, function factory( Flickity ) {
+}( window, function factory( Flickity ) {
 
-Flickity.create.fullscreen = function() {
+'use strict';
+
+Flickity.createMethods.push('_createFullscreen');
+var proto = Flickity.prototype;
+
+
+proto._createFullscreen = function() {
   this.isFullscreen = false;
 
-  if ( !this.options.fullscreen ) return;
+  if ( !this.options.fullscreen ) {
+    return;
+  }
   // buttons
   this.viewFullscreenButton = new FullscreenButton( 'view', this );
   this.exitFullscreenButton = new FullscreenButton( 'exit', this );
@@ -27,16 +47,14 @@ Flickity.create.fullscreen = function() {
   this.on( 'deactivate', this._changeFullscreenActive );
 };
 
-let proto = Flickity.prototype;
-
 // ----- activation ----- //
 
 proto._changeFullscreenActive = function() {
-  let childMethod = this.isActive ? 'appendChild' : 'removeChild';
+  var childMethod = this.isActive ? 'appendChild' : 'removeChild';
   this.element[ childMethod ]( this.viewFullscreenButton.element );
   this.element[ childMethod ]( this.exitFullscreenButton.element );
   // activate or deactivate buttons
-  let activeMethod = this.isActive ? 'activate' : 'deactivate';
+  var activeMethod = this.isActive ? 'activate' : 'deactivate';
   this.viewFullscreenButton[ activeMethod ]();
   this.exitFullscreenButton[ activeMethod ]();
 };
@@ -53,15 +71,18 @@ proto.exitFullscreen = function() {
 };
 
 proto._changeFullscreen = function( isView ) {
-  if ( this.isFullscreen === isView ) return;
-
+  if ( this.isFullscreen == isView ) {
+    return;
+  }
   this.isFullscreen = isView;
-  let classMethod = isView ? 'add' : 'remove';
+  var classMethod = isView ? 'add' : 'remove';
   document.documentElement.classList[ classMethod ]('is-flickity-fullscreen');
   this.element.classList[ classMethod ]('is-fullscreen');
   this.resize();
   // HACK extra reposition on fullscreen for images
-  if ( this.isFullscreen ) this.reposition();
+  if ( this.isFullscreen ) {
+    this.reposition();
+  }
   this.dispatchEvent( 'fullscreenChange', null, [ isView ] );
 };
 
@@ -72,7 +93,7 @@ proto.toggleFullscreen = function() {
 // ----- setGallerySize ----- //
 
 // overwrite so fullscreen cells are full height
-let setGallerySize = proto.setGallerySize;
+var setGallerySize = proto.setGallerySize;
 proto.setGallerySize = function() {
   if ( !this.options.setGallerySize ) {
     return;
@@ -108,35 +129,35 @@ function FullscreenButton( name, flickity ) {
 }
 
 FullscreenButton.prototype.createButton = function() {
-  let element = this.element = document.createElement('button');
+  var element = this.element = document.createElement('button');
   element.className = 'flickity-button flickity-fullscreen-button ' +
     'flickity-fullscreen-button-' + this.name;
   // prevent button from submitting form
   element.setAttribute( 'type', 'button' );
   // set label
-  let label = capitalize( this.name + ' full-screen' );
+  var label = capitalize( this.name + ' full-screen' );
   element.setAttribute( 'aria-label', label );
   element.title = label;
 };
 
 function capitalize( text ) {
-  return text[0].toUpperCase() + text.slice( 1 );
+  return text[0].toUpperCase() + text.slice(1);
 }
 
-const svgURI = 'http://www.w3.org/2000/svg';
+var svgURI = 'http://www.w3.org/2000/svg';
 
-const pathDirections = {
+var pathDirections = {
   view: 'M15,20,7,28h5v4H0V20H4v5l8-8Zm5-5,8-8v5h4V0H20V4h5l-8,8Z',
   exit: 'M32,3l-7,7h5v4H18V2h4V7l7-7ZM3,32l7-7v5h4V18H2v4H7L0,29Z',
 };
 
 FullscreenButton.prototype.createIcon = function() {
-  let svg = document.createElementNS( svgURI, 'svg' );
+  var svg = document.createElementNS( svgURI, 'svg');
   svg.setAttribute( 'class', 'flickity-button-icon' );
   svg.setAttribute( 'viewBox', '0 0 32 32' );
   // path & direction
-  let path = document.createElementNS( svgURI, 'path' );
-  let direction = pathDirections[ this.name ];
+  var path = document.createElementNS( svgURI, 'path');
+  var direction = pathDirections[ this.name ];
   path.setAttribute( 'd', direction );
   // put it together
   svg.appendChild( path );
@@ -157,4 +178,4 @@ Flickity.FullscreenButton = FullscreenButton;
 
 return Flickity;
 
-} ) );
+}));
